6f33fcaf1e34ccdd3f433e14921bcfc71e4b8d58
William Roton <william.roton@hotmail.com>
2020-01-28 00:30:05 -0600

    Set origin

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -33,7 +33,7 @@ export class Request implements RequestInit {
 
   constructor(route: string, method: Method, body?: object) {
     // Set standard properties.
-    this.url = "http://localhost" + route;
+    this.url = window.location.origin + route;
     this.method = method.toString();
 
     // Serialize the body.
